476bf5b3218c1603cef0e9e5c353c9af569696ae
hanjy-slowturtle <h920915@gmail.com>
2022-02-04 00:00:18 +0900

    ~ #1.0 React + Firebase Setup

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.5/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBnc9PPKESk30ONWNUggMXjyNXoZXdjTPQ",
+  authDomain: "nwitter2-431ef.firebaseapp.com",
+  projectId: "nwitter2-431ef",
+  storageBucket: "nwitter2-431ef.appspot.com",
+  messagingSenderId: "968048929413",
+  appId: "1:968048929413:web:9e8f5a713f94f39fa957ec",
+};
+
+// Initialize Firebase
+export default initializeApp(firebaseConfig);
